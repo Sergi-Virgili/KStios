d40c5c805c0ebf4c03bce25469c09f4718b427f3
Sergi-Virgili <svirgilif@gmail.com>
2024-01-29 20:44:25 +0100

    Add textToJSON2 function to convert text to JSON format

--- src/spikes/realtimeConverter.ts
+++ src/spikes/realtimeConverter.ts
@@ -65,3 +65,71 @@ export function textToJSON(texto: string) {
     });
   return questions;
 }
+
+export function textToJSON2(texto: string) 
+  {
+  const questions = texto
+    // .replace(/\*/g, "")
+    .replaceAll("[ ]", "")
+
+    .split("Pregunta")
+    .splice(1)
+    .map((question) => {
+      const pregunta = question
+        .split("Explicación")[0]
+        .replaceAll("**", "")
+        .split("•")[0]
+        .trim()
+        .slice(3)
+        .replace("Omitido", "")
+        .replace("Correcto", "")
+        .trim();
+
+      const respuestas = question
+        .split("Explicación")[0]
+        .replaceAll("**", "")
+        .split("•")
+        .filter((_x, index) => index != 0)
+        .map((answer) => {
+          const isCorrect = answer.includes("(Correcto)");
+          const texto = answer.replace("\n", "").trim().replace("(Correcto)", "");
+          return {
+            answer: texto,
+            correct: isCorrect,
+          };
+        });
+
+      return {
+        question: pregunta.replaceAll("\n", "<br />").replaceAll('"', ""),
+        answers: respuestas,
+        explanation: question
+          .slice(
+            question.indexOf("Opción correcta:"),
+            question.indexOf("Opciones incorrectas:")
+          )
+          .replace("Opción correcta:", "")
+          .trim()
+          .replaceAll("\n", "<br />")
+          .replaceAll("**", "")
+          .replace('"', ""),
+        incorrect: question
+          .slice(
+            question.indexOf("Opciones incorrectas:"),
+            question.indexOf("Referencia:")
+          )
+          .replace("Opciones incorrectas:", "")
+          .trim()
+          .replaceAll("**", "<br />")
+          .replaceAll('"', ""),
+        references: question
+          .slice(question.indexOf("Referencia:"))
+          .replace("Referencia:", "")
+          .trim()
+          .replaceAll("\n", "<br />")
+          .replaceAll('"', ""),
+      };
+    });
+  // return JSON.stringify(questions)
+  return questions;
+}
+
